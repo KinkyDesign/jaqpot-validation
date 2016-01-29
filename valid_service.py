19b2b7ee5a1021bf795fbf6312d129bad967a601
gdrakakis <gdrakakis356@gmail.com>
2016-01-29 12:41:18 +0200

    cleaned code/added plots

--- valid_service.py
+++ valid_service.py
@@ -30,12 +30,15 @@ from scipy.stats import chisquare
 from copy import deepcopy
 import operator 
 import matplotlib
+import io
 from io import BytesIO
 #matplotlib.use('Agg')
 import matplotlib.pyplot
 import matplotlib.pyplot as plt
 from operator import itemgetter
+
 #from PIL import Image ## Hide for production
+
 app = Flask(__name__, static_url_path = "")
 
 """
@@ -72,24 +75,150 @@ def getJsonContents (jsonInput):
         
     except(ValueError, KeyError, TypeError):
         print "Error: Please check JSON syntax... \n"
-    print len(real), len(predicted)
+    #print len(real), len(predicted)
     return real, predicted, type, number_of_variables, predictionFeature, predictedFeature
 
-
-def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+"""
+    Matplotlib default Confusion Matrix
+"""
+def plot_confusion_matrix(cm, labels, title='Confusion matrix', cmap=plt.cm.Blues):
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
-    #tick_marks = np.arange(len(iris.target_names))
-    #plt.xticks(tick_marks, iris.target_names, rotation=45)
-    #plt.yticks(tick_marks, iris.target_names)
+    tick_marks = numpy.arange(len(labels))
+    plt.xticks(tick_marks, labels)
+    plt.yticks(tick_marks, labels)
+
+    plt.xlabel('True label')
+    plt.ylabel('Predicted label')
+	
     plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
 
+"""
+    QQ plot of Real Quantiles vs Predicted Quantiles
+"""
+def qq_plot(real, predicted):
+    rp = zip (real, predicted)
+    rp.sort()
+    real_sorted = [r for r,p in rp]
+    predicted_sorted = [p for r,p in rp]
 
+    real_sorted = 1.0*numpy.array(real_sorted)
+    predicted_sorted = 1.0*numpy.array(predicted_sorted)
+
+    # Each of Real/Predicted with Theoretical Quantiles
+    """
+    myFIGA = plt.figure()
+    ax = myFIGA.add_subplot(111)
+    scipy.stats.probplot(real_sorted, dist="norm", plot=ax)
+    #plt.show()
+    plt.close()
+	
+    myFIGA = plt.figure()
+    ax = myFIGA.add_subplot(111)
+    scipy.stats.probplot(predicted_sorted, dist="norm", plot=ax)
+    #plt.show()
+    plt.close()
+    """
+
+    myFIGA = plt.figure()
+
+    real_intervals = [(x-min(real_sorted))*100/(max(real_sorted)-min(real_sorted)) for x in real_sorted]
+    #predicted_intervals = [(x-min(predicted_sorted))*100/(max(predicted_sorted)-min(predicted_sorted)) for x in predicted_sorted]
+    #print real_intervals, "\n", predicted_intervals, "\n"
+
+    real_percentile = numpy.percentile(real_sorted, real_intervals)
+    #predicted_percentile = numpy.percentile(predicted_sorted, predicted_intervals) ####
+    predicted_percentile = numpy.percentile(predicted_sorted, real_intervals) ####
+    #print real_percentile, "\n", predicted_percentile
+	
+    plt.plot(real_percentile, predicted_percentile, 'ro', c="red")
+    straight, = plt.plot(real_percentile, real_percentile, 'r', c="green", label = "Quantile Identity Line")
+
+    adjustment1 = abs(max(real_percentile) - min (real_percentile))*0.05 # +/- 5%
+    adjustment2 = abs(max(predicted_percentile) - min (predicted_percentile))*0.05 # +/- 5%
+    plt.xlim([round(min(real_percentile)) - adjustment1, round(max(real_percentile)) + adjustment1,])
+    plt.ylim([round(min(predicted_percentile)) - adjustment2, round(max(predicted_percentile)) + adjustment2,])
+
+    plt.xlabel("Quantiles for Real Values")
+    plt.ylabel("Quantiles for Predicted  Values")
+	
+    plt.title('QQ Plot')
+    myLegend = plt.legend(handles = [straight], loc=2, fontsize = 'small')
+	
+    plt.tight_layout()
+    #plt.show() ## HIDE show on production
+
+    #sio = cStringIO.StringIO()
+    sio = BytesIO()
+    myFIGA.savefig(sio, dpi=300, format='png') # (sio, dpi=300, format='png', bbox_extra_artists=(myLegend,), bbox_inches='tight')
+    saveas = pickle.dumps(sio.getvalue())
+    fig_encoded = base64.b64encode(saveas)
+
+    plt.close()
+    return fig_encoded
+
+"""
+    Get % Confidence Interval for predictions
+"""
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*numpy.array(data)
+    n = len(a)
+    m, se = numpy.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
+    return m, h, -h
+
+"""
+    Plot Real vs. predicted values
+"""
+def plot_norm (real, predicted):
+    rp = zip (real, predicted)
+    rp.sort()
+    real_sorted = [r for r,p in rp]
+    predicted_sorted = [p for r,p in rp]
+    #print real_sorted, "\n", predicted_sorted
+    
+
+    myFIGA = plt.figure()
+	
+    m, m_plus, m_minus = mean_confidence_interval(real_sorted)
+    plus = [x + m_plus for x in real_sorted]
+    minus = [x + m_minus for x in real_sorted]
+	
+    plt.plot(real_sorted, predicted_sorted, 'ro', c="red")
+    straight, = plt.plot(real_sorted, real_sorted, 'r', c="green", label = "Identity Line (Real = Predicted)")
+    dashed, = plt.plot(real_sorted, plus, 'r--', c="green", label = "95% Confidence Level")
+    plt.plot(real_sorted, minus, 'r--', c="green")
+	
+    adjustment = abs(max(real_sorted) - min (real_sorted))*0.05 # +/- 5%
+    plt.xlim([round(min(real_sorted)) - adjustment, round(max(real_sorted)) + adjustment,])
+    plt.ylim([round(min(real_sorted)) - adjustment, round(max(real_sorted)) + adjustment,])
+
+    plt.xlabel("Real Values")
+    plt.ylabel("Predicted Values")
+	
+    plt.title('Real vs Predicted Values')
+    myLegend = plt.legend(handles = [straight, dashed], loc=2, fontsize = 'small')
+	
+    plt.tight_layout()
+    #plt.show() ## HIDE show on production
+
+    #sio = cStringIO.StringIO()
+    sio = BytesIO()
+    myFIGA.savefig(sio, dpi=300, format='png') # myFIGA1a.savefig(sio, dpi=300, format='png', bbox_extra_artists=(myLegend,), bbox_inches='tight')
+    saveas = pickle.dumps(sio.getvalue())
+    fig_encoded = base64.b64encode(saveas)
+	
+    plt.close()
+    return fig_encoded
+
+"""
+    Get Regression Stats
+"""
 def stats_regression(Y, predY, num_predictors):
-
+    fig1 = plot_norm(Y, predY)
+    fig2 = qq_plot(Y, predY) ##
+	
     meanY4r2 = numpy.mean(Y)
     meanYpred4r2 = numpy.mean(predY)
 
@@ -125,57 +254,132 @@ def stats_regression(Y, predY, num_predictors):
     StdError = numpy.sqrt(abs(RSS/(len(Y)-num_predictors-1)))
     Fvalue = (SSR/num_predictors)/(RSS/(len(Y)-num_predictors-1))
 
-    #add errors from sklearn
+    ##add errors from sklearn
 
-    return R2wolfram, R2adjusted, RMSD, Fvalue, StdError
+    return round(R2wolfram,2), round(R2adjusted,2), round(RMSD,2), round(Fvalue,2), round(StdError,2), fig1, fig2
 
-
+"""
+    Get Classification Stats
+"""
 def stats_classification(Y, predY):
     Accuracy = sklearn.metrics.accuracy_score(Y, predY) #, normalize=True, sample_weight=None) #pos label 1 deleted
     Precision = sklearn.metrics.precision_score(Y, predY, pos_label=None)#, labels=None, average='binary', sample_weight=None)
     Recall = sklearn.metrics.recall_score(Y, predY, pos_label=None)#, labels=None, average='binary', sample_weight=None)
     F1_score = sklearn.metrics.f1_score(Y, predY, pos_label=None)#, labels=None, average='binary', sample_weight=None)
     Jacc = sklearn.metrics.jaccard_similarity_score(Y, predY) 
-    #Precision = 0
-    #Recall = 0
-    #F1_score = 0
-    #Jacc = 0 
-    #TP.TN/FP/FN - > MCC etc ?
 
-    """
-    # Check if Y is binary and convert Y, predY to True/False
+    ## General case for roc/auc
     AUC_decision = []
-    for i in range (len(Y)):
-        if Y[i] == predY[i]:
-            AUC_decision.append(True)
-        else: 
-            AUC_decision.append(False)
-    AUC = sklearn.metrics.roc_auc_score(Y, AUC_decision),  average='macro', sample_weight=None)
-    """
+    editedY = []
+    indices = list(set(Y)) 
+    for i in range (len(indices)):
+        editedY.append([])
+        AUC_decision.append([])
 
-    cm = confusion_matrix(Y, predY)
+    for i in range (len(Y)):
+        for j in range (len(indices)):
+            if Y[i] == indices[j]: 
+                editedY[j].append(0) 
+                if predY[i] == indices[j]: 
+                    AUC_decision[j].append(0) 
+                else: 
+                    AUC_decision[j].append(1) 
+            else:
+                editedY[j].append(1) 
+                if predY[i] == indices[j]: 
+                    AUC_decision[j].append(0) 
+                else: 
+                    AUC_decision[j].append(1) 
+
+    fpr = []
+    tpr = []
+    thresholds = []
+    for i in range (len(indices)):
+            fpr.append([])
+            tpr.append([])
+            thresholds.append([])
+
+
+    for i in range (len(indices)):
+        fpr[i], tpr[i], thresholds[i] = sklearn.metrics.roc_curve(editedY[i], AUC_decision[i])
+
+    myROC = plt.figure()
+
+    for i in range(len(indices)):
+        plt.plot(fpr[i], tpr[i], label='TP/FP rates for Class: '+str(indices[i])) ## Change Message if ROC
+
+    #all_fpr = numpy.unique(numpy.concatenate([fpr[i] for i in range(len(indices))]))
+    #all_tpr = numpy.unique(numpy.concatenate([tpr[i] for i in range(len(indices))]))
+	
+    #all_fpr = numpy.sort(numpy.concatenate([fpr[i] for i in range(len(indices))]))
+    #all_tpr = numpy.sort(numpy.concatenate([tpr[i] for i in range(len(indices))]))
+	
+    #plt.plot(all_fpr, all_tpr, label='Max ROC')
+
+    plt.plot([0, 1], [0, 1], 'k--') # y = x
+    plt.xlim([-0.05, 1.05]) # +/- 0.05
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC-like Representation of Confusion Matrix') ## Change Message if ROC
+    plt.legend(loc="lower right")
+
+    plt.tight_layout()
+
+    figfileROC = BytesIO()
+    myROC.savefig(figfileROC, dpi=300, format='png')
+    saveas = pickle.dumps(figfileROC.getvalue())
+    roc_encoded = base64.b64encode(saveas)
+
+    #figfileROC = BytesIO()
+    #plt.savefig(figfileROC, format='png')
+    #figfileROC.seek(0)  # rewind to beginning of file
+    #roc_encoded = base64.b64encode(figfileROC.getvalue())
+
+    #plt.show() ##
+    plt.close()
+    ## end general case roc/auc
+
+    # DEBUG Conf Mat
+    #from collections import Counter
+    #print Y, "\n", predY, "\n", list(set(Y))
+    #print Counter(Y), Counter(predY)
+
+    cm = confusion_matrix(Y, predY, labels = list(set(Y)))
     numpy.set_printoptions(precision=2)
 
     myFIGA = plt.figure()
-    plot_confusion_matrix(cm, title='Confusion matrix')
+    plot_confusion_matrix(cm, list(set(Y)), title='Confusion matrix')
 
+    plt.tight_layout()
+
+    ## String IO
     #sio = cStringIO.StringIO()
     #myFIGA.savefig(sio, dpi=300, format='png') 
     #saveas = pickle.dumps(sio.getvalue())
     #cm_encoded = base64.b64encode(saveas)
 
-    figfile = BytesIO()
-    plt.savefig(figfile, format='png')
-    figfile.seek(0)  # rewind to beginning of file
-    cm_encoded = base64.b64encode(figfile.getvalue())
+    ## Bytes IO
+    bio = BytesIO()
+    myFIGA.savefig(bio, dpi=300, format='png')
+    saveas = pickle.dumps(bio.getvalue())
+    cm_encoded = base64.b64encode(saveas)
 
-    plt.show()
+    ## Bytes IO v2
+    #figfile = BytesIO()
+    #plt.savefig(figfile, format='png')
+    #figfile.seek(0)  # rewind to beginning of file
+    #cm_encoded = base64.b64encode(figfile.getvalue())
+
+    #plt.show() ## show CM
     plt.close()
 
     ## can plot normalized cm as well
     #cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, numpy.newaxis]
     #plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
-    return Accuracy, Precision, Recall, F1_score, Jacc, cm_encoded
+
+    return round(Accuracy,2), round(Precision,2), round(Recall,2), round(F1_score,2), round(Jacc,2), cm_encoded, roc_encoded
+
 
 """
     [[],[]] Transposed Matrix to dictionary
@@ -201,8 +405,8 @@ def create_task_interlabtest():
     full_table_dict = mat2dic(full_table_transposed)
     #print full_table_dict
 
-    if type == "REGRESSION":
-        R2wolfram, R2adjusted, RMSD, Fvalue, StdError = stats_regression(real, predicted, number_of_variables)
+    if type == "REGRESSION" and (max(real)-min(real)!=0) and (max(predicted)-min(predicted)!=0):
+        R2wolfram, R2adjusted, RMSD, Fvalue, StdError, fig1, fig2 = stats_regression(real, predicted, number_of_variables)
         task = {
         "singleCalculations": {"Algorithm Type": type, 
                                "Number of predictor variables": number_of_variables,
@@ -218,10 +422,12 @@ def create_task_interlabtest():
                                }
                              },
         "figures": {
+                   "Real Vs Predicted" : "fig1",
+                   "QQ Plot" : "fig2"
                    }
         }
     elif type == "CLASSIFICATION":
-        Accuracy, Precision, Recall, F1_score, Jaccard, cm_encoded = stats_classification(real, predicted)
+        Accuracy, Precision, Recall, F1_score, Jaccard, cm_encoded, roc_encoded = stats_classification(real, predicted)
         task = {
         "singleCalculations": {"Algorithm Type": type, 
                                "Number of predictor variables": number_of_variables,
@@ -236,12 +442,49 @@ def create_task_interlabtest():
                                 "values": full_table_dict
                                }
                              },
-        "figures": {"Confusion Matrix" : cm_encoded
+        "figures": {
+                   #"ROC-like Curve for TP/FP rates" : roc_encoded,
+                   "Confusion Matrix" : cm_encoded
+                   }
+        }
+    else:
+        task = {
+        "singleCalculations": {"Validation Failed": "Check Dataset"
+                              },
+        "arrayCalculations": {"Reasons Include" : 
+                               {"colNames": ["Real", "Predicted"],
+                                "values": {
+                                           "Row_1" : ["1", "Empty Dataset"],
+                                           "Row_2" : ["2", "Prediction Feature Values Identical"],
+                                           "Row_3" : ["3", "Predicted Feature Values Identical"]
+                                          }
+                               }
+                             },
+        "figures": {
                    }
         }
 
     jsonOutput = jsonify( task )
 
+    ## DEBUG 
+    # REGRESSION IMAGES
+    """
+    decc = base64.standard_b64decode(fig1) 
+    mystr = pickle.loads(decc)
+    stb = io.BytesIO(mystr)
+    img = Image.open(stb)
+    img.seek(0)
+    img.save('C:/Python27/Flask-0.10.1/python-api/Val/rvp.png', 'png')
+    """
+    # CLASSIFICATION IMAGES
+    """
+    decc = base64.standard_b64decode(cm_encoded) 
+    mystr = pickle.loads(decc)
+    stb = io.BytesIO(mystr)
+    img = Image.open(stb)
+    img.seek(0)
+    img.save('C:/Python27/Flask-0.10.1/python-api/Val/confmat.png', 'png')
+    """
     return jsonOutput, 201 
 
 if __name__ == '__main__': 
